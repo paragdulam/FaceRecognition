3ebe75999fd024f230d61b621c47134b806dcb85
Parag Dulam <paragdulam@gmail.com>
2015-01-27 00:07:55 +0530

    Initial Commit

---
+++ FaceRecognition/FaceRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceRecognition
+//
+//  Created by Parag Dulam on 27/01/15.
+//  Copyright (c) 2015 Parag Dulam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
